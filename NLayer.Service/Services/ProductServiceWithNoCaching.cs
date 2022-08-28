b464b91e3fc90fb85908e08cf9d46334cc9b3000
Sena Gül <sena.gul@ideabilisim.com>
2022-08-28 15:21:57 +0300

    API Caching

--- NLayer.Service/Services/ProductServiceWithNoCaching.cs
+++ NLayer.Service/Services/ProductServiceWithNoCaching.cs
@@ -12,12 +12,12 @@ using System.Threading.Tasks;
 
 namespace NLayer.Service.Services
 {
-    public class ProductService : Service<Product>, IProductService
+    public class ProductServiceWithNoCaching : Service<Product>, IProductService
     {
 
         private readonly IMapper _mapper;
         private readonly IProductRepository _productRepository;
-        public ProductService(IGenericRepository<Product> repository, IUnitOfWork unitOfWork, IMapper mapper, IProductRepository productRepository) : base(repository, unitOfWork)
+        public ProductServiceWithNoCaching(IGenericRepository<Product> repository, IUnitOfWork unitOfWork, IMapper mapper, IProductRepository productRepository) : base(repository, unitOfWork)
         {
             _mapper = mapper;
             _productRepository = productRepository;
